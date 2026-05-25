4e8005cad59666365b5c77f3189b973acfb594d8
srinisha-sriramula <srinishasriramula29@gmail.com>
2026-05-26 01:14:22 +0530

    react

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
 import Post from "./Post";
+import WelcomeMessage from "./WelcomeMessage";
 const PostList = () => {
   const { postList } = useContext(PostListData);
   return (
     <>
+    {postList.length===0 && <WelcomeMessage></WelcomeMessage>}
       {postList.map((post) => (
         <Post key={post.id} post={post}></Post>
       ))}
