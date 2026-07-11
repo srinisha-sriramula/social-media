f6bbcb2878b3a4036afc5d0c7a3c016882d33550
srinisha-sriramula <srinishasriramula29@gmail.com>
2026-07-11 16:20:27 +0530

    loading

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,20 +2,28 @@ import { useContext, useEffect, useState } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
 import Post from "./Post";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+
+  const [fetching,setFetching]=useState(false);
+
+
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data)=>{
         addInitialPosts(data.posts);
+        setFetching(false);
       });
   },[]);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-      {postList.map((post) => (
+      {fetching && <LoadingSpinner></LoadingSpinner>}
+      {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {!fetching && postList.map((post) => (
         <Post key={post.id} post={post}></Post>
       ))}
     </>
