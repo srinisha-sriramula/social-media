d5095acbfd2a8ba0037ce8bfb8b1ec0339999ee7
srinisha-sriramula <srinishasriramula29@gmail.com>
2026-07-15 21:18:49 +0530

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+const router = createBrowserRouter([
+  { path: "/", element: <App></App> },
+  { path: "/create-post", element: <CreatePost /> },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
-)
+);
